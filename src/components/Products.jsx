821f2f92480c2d323b85b184b44165006807a5ce
majifo99 <135087859+majifo99@users.noreply.github.com>
2024-04-24 15:49:06 -0600

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+const apiUrl = 'https://api.escuelajs.co/api/v1/products';
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className='results-container'>
+      <div className='results'>
+        {products.map(product => (
+          <div className="result-item" key={product.id}>
+            <div className="card border-primary" style={{ maxWidth: '200px' }}>
+              <img src={product.images[0]} alt={product.title} className="card-img-top" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">Price: ${product.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default Products;
